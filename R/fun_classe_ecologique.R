e32e25a409c19bd050eb19f6bf8c82315c3172c5
julia-mourgues <julia.mourgues@etudiant.univ-lr.fr>
2026-07-06 10:28:41 +0200

    Modifications

--- R/fun_classe_ecologique.R
+++ R/fun_classe_ecologique.R
@@ -1,27 +1,28 @@
-# Attribuer les classes d'état écologiques et les couleurs correspondantes aux résultats des EQR
+#' Attribuer les classes d'état écologiques et les couleurs correspondantes aux résultats des EQR
 
 valeurs_seuils <- readRDS(here::here("Data/clean_data", "valeurs_seuils_indices.rds"))
 
 
-attribuer_classe_eco <- function(data, table_seuils=valeurs_seuils, var_eqr, var_temp) {
-  
-  data_classe <- data%>%
+attribuer_classe_eco <- function(data, 
+                                 table_seuils = valeurs_seuils, 
+                                 var_eqr, var_temp) {
+  data_classe <- data %>%
     dplyr::left_join(
       table_seuils,
       by = c(
         "code_station"   = "CODE_STATION",
         "libelle_indice" = "indice",
         "Typologie"      = "TYPO_NATIONALE"
-      )
+      ),
+      relationship = "many-to-many"
     ) %>%
-    dplyr::filter(
-      .data[[var_eqr]] >= seuil_bas &
-      .data[[var_eqr]] <= seuil_haut
-    ) %>%
-    dplyr::distinct(
-      code_station, libelle_indice, Typologie, .data[[var_temp]],
-      .keep_all = TRUE
-    )
+    dplyr::filter(.data[[var_eqr]] >= seuil_bas &
+                    .data[[var_eqr]] <= seuil_haut) %>%
+    dplyr::distinct(code_station, 
+                    libelle_indice, 
+                    Typologie, 
+                    .data[[var_temp]], 
+                    .keep_all = TRUE)
   return(data_classe)
 }
 
@@ -33,11 +34,10 @@ attribuer_classe_eco <- function(data, table_seuils=valeurs_seuils, var_eqr, var
 #' @param bornes_indices listes contenant les bornes inférieures et supérieures des classes de chaque indice
 
 creation_classes <- function(col_code, bornes_indices) {
-  
   b <- bornes_indices[[col_code]]
   
-  # Libellés homogènes 
-  classe_libelle <- c("Mauvais", "Médiocre", "Moyen", "Bon", "Très bon") 
+  # Libellés homogènes
+  classe_libelle <- c("Mauvais", "Médiocre", "Moyen", "Bon", "Très bon")
   classe_couleur <- b$couleurs
   
   # Inversion de la légende UNIQUEMENT pour 7036
@@ -47,7 +47,7 @@ creation_classes <- function(col_code, bornes_indices) {
   }
   
   data.frame(
-    libelle_indice = col_code, 
+    libelle_indice = col_code,
     classe_borne_inf = b$borne_inf,
     classe_borne_sup = b$borne_sup,
     classe_libelle = factor(
@@ -58,5 +58,3 @@ creation_classes <- function(col_code, bornes_indices) {
     stringsAsFactors = FALSE
   )
 }
-
-
--- R/fun_graph_barplot_proportion.R
+++ R/fun_graph_barplot_proportion.R
@@ -8,13 +8,15 @@ barplot_proportion <- function(data,
                                liste_reseaux,
                                nb_min_station,
                                output_dir = "../Output/4_Proportions_classes_eco/01_Barplot") {
-  
-  #Construire automatiquement le "titre réseau"
+
+    #Construire automatiquement le "titre réseau"
   reseau_titre <- paste(sort(unique(liste_reseaux)), collapse = "_")
   
   # S'assurer que le répertoire de sortie existe
-  if (!dir.exists(output_dir)) dir.create(output_dir, recursive = TRUE)
+  if (!dir.exists(output_dir))
+    dir.create(output_dir, recursive = TRUE)
   
+  #filtrer et réarranger les données
   data_filtre <- data %>%
     dplyr::ungroup() %>%
     dplyr::filter(.data$reseaux %in% liste_reseaux)
@@ -27,12 +29,14 @@ barplot_proportion <- function(data,
   data_filtre2 <- data_filtre %>%
     dplyr::inner_join(nb_annee, by = c("libelle_indice", "annee"))
   
+  #calcul des proportions
   data_pct <- data_filtre2 %>%
     dplyr::group_by(code_indice, libelle_indice, annee, libelle_classe) %>%
-    dplyr::summarise(n_classe = dplyr::n_distinct(code_station), .groups = "drop") %>%
+    dplyr::summarise(n_classe = dplyr::n_distinct(code_station),
+                     .groups = "drop") %>%
     dplyr::group_by(code_indice, libelle_indice, annee) %>%
     dplyr::mutate(n_total = sum(n_classe),
-           pct = (n_classe / n_total) * 100) %>%
+                  pct = (n_classe / n_total) * 100) %>%
     dplyr::ungroup()
   
   data_pct$libelle_classe <- factor(
@@ -41,17 +45,21 @@ barplot_proportion <- function(data,
     ordered = TRUE
   )
   
-  
-  couleurs  = c("red","orange","yellow","lightgreen","#2b83ba")
+  #définir les classes d'état et leur couleur
+  couleurs  = c("red", "orange", "yellow", "lightgreen", "#2b83ba")
   classes   = c("Mauvais", "Médiocre", "Moyen", "Bon", "Très bon")
   palette_etat <- setNames(couleurs, classes)
   
+  
+  #graphique présentant le pourcentage de station dans chaque état biologique au cours du temps
   g1 <- ggplot2::ggplot(data_pct, ggplot2::aes(x = annee, y = pct, fill = libelle_classe)) +
     ggplot2::geom_col(position = "fill") +
-    ggplot2::facet_wrap( ~ libelle_indice, ncol = 4) +
+    ggplot2::facet_wrap(~ libelle_indice, ncol = 4) +
     ggplot2::scale_y_continuous(labels = scales::percent_format()) +
-    scale_fill_manual(values = palette_etat, 
-                      breaks = c("Très bon", "Bon", "Moyen", "Médiocre", "Mauvais") ) + 
+    scale_fill_manual(
+      values = palette_etat,
+      breaks = c("Très bon", "Bon", "Moyen", "Médiocre", "Mauvais")
+    ) +
     ggplot2::theme_bw() +
     ggplot2::labs(
       title = paste0(
@@ -63,7 +71,8 @@ barplot_proportion <- function(data,
         "Les histogrammes tracés en dessous présentent le nombre de stations prises en comptes pour le calcul des proportions.
          Les années avec moins de ",
         nb_min_station,
-        " stations n'ont pas été prises en compte."),
+        " stations n'ont pas été prises en compte."
+      ),
       x = "",
       y = "Proportion",
       fill = "Etat écologique"
@@ -81,21 +90,24 @@ barplot_proportion <- function(data,
   data_n <- data_pct %>%
     dplyr::distinct(annee, libelle_indice, n_total)
   
+  
+  #graphique présentant le nombre total de stations prises en compte par année
   g2 <- ggplot(data_n, aes(x = annee, y = n_total)) +
     ggplot2::geom_col(fill = "grey", color = "black") +
-    ggplot2::facet_wrap( ~ libelle_indice, ncol = 4) +
-    ggplot2::labs(subtitle = " ", 
-                  x = "Année", 
-                  y = "Nombre de stations") +
+    ggplot2::facet_wrap(~ libelle_indice, ncol = 4) +
+    ggplot2::labs(subtitle = " ", x = "Année", y = "Nombre de stations") +
     ggplot2::theme_bw()
   
-  distrib <- patchwork::wrap_plots(g1, 
-                                   g2, 
-                                   ncol = 1, 
-                                   heights = c(2.5, 1.5))
+  #Assembler les deux graphiques
+  distrib <- patchwork::wrap_plots(g1, g2, ncol = 1, heights = c(2.5, 1.5))
   
+  
+  #Télécharger la figure
   ggplot2::ggsave(
-    filename = file.path(output_dir, paste0("proportions_classe_",reseau_titre,".jpg")),
+    filename = file.path(
+      output_dir,
+      paste0("proportions_classe_", reseau_titre, ".jpg")
+    ),
     plot = distrib,
     width = 14,
     height = 9,
@@ -113,15 +125,13 @@ barplot_proportion <- function(data,
 #' @param liste_reseaux liste des réseaux étudiés
 
 
-barplot_nombre <- function(data,
-                           liste_reseaux,
-                           output_dir = "../Output/4_Proportions_classes_eco/01_Barplot") {
-  
+barplot_nombre <- function(data, liste_reseaux, output_dir = "../Output/4_Proportions_classes_eco/01_Barplot") {
   #Construire automatiquement le "titre réseau"
   reseau_titre <- paste(sort(unique(liste_reseaux)), collapse = "_")
   
   # S'assurer que le répertoire de sortie existe
-  if (!dir.exists(output_dir)) dir.create(output_dir, recursive = TRUE)
+  if (!dir.exists(output_dir))
+    dir.create(output_dir, recursive = TRUE)
   
   data_filtre <- data %>%
     dplyr::ungroup() %>%
@@ -134,7 +144,8 @@ barplot_nombre <- function(data,
   
   data_counts <- data_filtre %>%
     dplyr::group_by(code_indice, libelle_indice, annee, libelle_classe) %>%
-    dplyr::summarise(n_classe = dplyr::n_distinct(code_station), .groups = "drop")
+    dplyr::summarise(n_classe = dplyr::n_distinct(code_station),
+                     .groups = "drop")
   
   
   data_counts$libelle_classe <- factor(
@@ -143,18 +154,18 @@ barplot_nombre <- function(data,
     ordered = TRUE
   )
   
-  couleurs  = c("red","orange","yellow","lightgreen","#2b83ba")
+  couleurs  = c("red", "orange", "yellow", "lightgreen", "#2b83ba")
   classes   = c("Mauvais", "Médiocre", "Moyen", "Bon", "Très bon")
   palette_etat <- setNames(couleurs, classes)
   
-  g3 <- ggplot2::ggplot(data_counts, 
-                        ggplot2::aes(x = annee, 
-                                     y = n_classe, 
-                                     fill = libelle_classe)) +
+  g3 <- ggplot2::ggplot(data_counts,
+                        ggplot2::aes(x = annee, y = n_classe, fill = libelle_classe)) +
     ggplot2::geom_col(position = "stack") +
-    ggplot2::facet_wrap( ~ libelle_indice, ncol = 4) +
-    scale_fill_manual(values = palette_etat, 
-                      breaks = c("Très bon", "Bon", "Moyen", "Médiocre", "Mauvais") ) +     
+    ggplot2::facet_wrap(~ libelle_indice, ncol = 4) +
+    scale_fill_manual(
+      values = palette_etat,
+      breaks = c("Très bon", "Bon", "Moyen", "Médiocre", "Mauvais")
+    ) +
     ggplot2::theme_bw() +
     ggplot2::labs(
       title = paste0(
@@ -172,16 +183,18 @@ barplot_nombre <- function(data,
         face = "bold",
         margin = margin(b = 5)
       ),
-      plot.subtitle = ggtext::element_textbox_simple(size = 12, 
-                                                     margin = margin(b = 10)),
+      plot.subtitle = ggtext::element_textbox_simple(size = 12, margin = margin(b = 10)),
       legend.title = ggplot2::element_text(size = 12, face = "bold"),
       axis.title.x = ggplot2::element_text(size = 11),
       axis.title.y = ggplot2::element_text(size = 11)
     )
-
+  
   
   ggplot2::ggsave(
-    filename = file.path(output_dir, paste0("nb_station_classe_",reseau_titre,".jpg")),
+    filename = file.path(
+      output_dir,
+      paste0("nb_station_classe_", reseau_titre, ".jpg")
+    ),
     plot = g3,
     width = 13,
     height = 7,
